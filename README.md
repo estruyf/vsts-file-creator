d1d0eff610fd9759177127ddd3f919bab7bc43bd
Elio Struyf <info@estruyf.be>
2018-01-18 12:51:04 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# File Creator
+
+This is a task that can be used to create a file during a build or release process. With the task you can specify the file you want to create and its content.
+
+![File creator options](./images/file-creator-options.png)
+
+## Options
+
+### Overwrite file
+
+This allows you to specify if you want to overwrite the file if it already exists. By default this is set to false.
+
+### File path
+
+With this property you can specify the path of the file including its name. Example: `folder/filename.txt`.
+
+### File content
+
+Specify the content of the file you want to add.
+
+## Feedback
+
+Feedback is always welcome. Please submit it via creating an issue in the repository [issue list](https://github.com/estruyf/vsts-file-creator/issues).
---
+++ buildtask/index.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const tl = require('vsts-task-lib/task');
+const fs = require('fs');
+
+// Set the source path
+tl.setResourcePath(path.join(__dirname, 'task.json'));
+
+// Get the two property values
+const filepath = tl.getInput('filepath', true, true);
+const filecontent = tl.getInput('filecontent', true);
+const fileoverwrite = tl.getInput('fileoverwrite', true);
+
+console.log(`File path: ${filepath}`);
+// console.log(`File content: ${filecontent}`);
+
+/**
+ * Create the folders for the file
+ * @param {*} filePath 
+ */
+function ensureDirectoryExistence(filePath) {
+  var dirname = path.dirname(filePath);
+  // console.log(`Creating dir: ${dirname}`);
+  if (fs.existsSync(dirname)) {
+    return true;
+  }
+  ensureDirectoryExistence(dirname);
+  fs.mkdirSync(dirname);
+}
+
+/**
+ * Start processing the file
+ */
+if (filepath && filecontent) {
+  // Check if file exists
+  if (!tl.exist(filepath) || fileoverwrite) {
+    // Create the folder, if needed
+    ensureDirectoryExistence(filepath);
+    // Create the file
+    tl.writeFile(filepath, filecontent, 'utf8');
+    // Check if the file is created
+    if (tl.exist(filepath)) {
+      console.log('File created');
+    } else {
+      tl.error('File not created / overwritten');
+    }
+  } else {
+    tl.error('File already exists');
+  }
+}
